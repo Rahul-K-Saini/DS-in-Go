a76150fa56657ed4acb830d7182778c3e5662826
Rahul-K-Saini <rahul545436@gmail.com>
2024-12-16 00:14:04 +0530

    feat: binary-tree

--- main.go
+++ main.go
@@ -1,45 +1,18 @@
 package main
 
 import (
-	// doublylinkedlist "dsa-in-go/doubly-linkedlist"
-	"dsa-in-go/queue"
+	"dsa-in-go/trees"
 	"fmt"
-	// linkedlist "dsa-in-go/linked-list"
-	// stack "dsa-in-go/stack"
 )
 
 func main() {
-	fmt.Println("Queue")
-	q := queue.MakeQueue[int](5)
-	q.Add(1)
-	q.Add(2)
-	q.Add(3)
-	q.Add(4)
-	q.Add(5)
-
-	fmt.Println(q.Delete())
-	// stk := stack.MakeStack[int]()
-	// stk.Push(1)
-	// fmt.Println(stk.Peek())
-	// fmt.Println(stk.IsEmpty())
-	// fmt.Println(stk.Pop())
-	// fmt.Print(stk.IsEmpty())
-	// fmt.Println("Doubly Linked List")
-	// dll := doublylinkedlist.CreateDoublyLinkedList()
-	// // dll.AddFirst(1)
-	// // dll.AddFirst(2)
-	// // dll.AddFirst(3)
-	// // dll.AddLast(4)
-	// err := dll.DeleteAtPos(0)
-	// if err != nil {
-	//   fmt.Print("Error :",err)
-	// }
-	// dll.PrintLinkedList()
-	//
-	// fmt.Println("LINKED LIST")
-	// // head = head.AddFirst(0)
-	// // head.AddAtPosition(2,12)
-	// // head.AddLast(11)
-	// // head.PrintLinkedList()
-	// // fmt.Println("\nLength of linkedlist is ",head.GetLength())
+	fmt.Println("Binary Tree")
+	bt := trees.NewBinaryTree[int]()
+	var arr []int
+	for i := 0; i < 20; i++ {
+		arr = append(arr, i)
+	}
+	bt.MakeBinaryTree(arr)
+	bt.PrintTree()
+	fmt.Printf("The length of Binary Tree is %d\n", bt.GetLength())
 }
---
+++ trees/binaryTree.go
@@ -0,0 +1,177 @@
+package trees
+
+import (
+	"fmt"
+	"math"
+)
+
+type TreeNode[T comparable] struct {
+	data  T
+	left  *TreeNode[T]
+	right *TreeNode[T]
+}
+
+type BinaryTree[T comparable] struct {
+	root *TreeNode[T]
+}
+
+func NewBinaryTree[T comparable]() *BinaryTree[T] {
+	return &BinaryTree[T]{}
+}
+
+func (bt *BinaryTree[T]) MakeBinaryTree(arr []T) error {
+	if len(arr) == 0 || arr == nil {
+		return fmt.Errorf("input array is empty")
+	}
+	bt.root = &TreeNode[T]{
+		data: arr[0],
+	}
+	helper(bt.root, arr, 0)
+	return nil
+}
+
+func helper[T comparable](node *TreeNode[T], arr []T, index int) {
+	if node == nil || index >= len(arr) {
+		return
+	}
+
+	leftIndex := 2*index + 1
+	rightIndex := 2*index + 2
+
+	if leftIndex < len(arr) {
+		node.left = &TreeNode[T]{data: arr[leftIndex]}
+		helper(node.left, arr, leftIndex)
+	}
+
+	if rightIndex < len(arr) {
+		node.right = &TreeNode[T]{data: arr[rightIndex]}
+		helper(node.right, arr, rightIndex)
+	}
+}
+
+func (bt *BinaryTree[T]) PrintTree() {
+	if bt.root == nil {
+		panic("tree is empty cant print empty tree")
+	}
+	fmt.Println("Enter:")
+	fmt.Println("1 for pre-order traversal")
+	fmt.Println("2 for post-order traversal")
+	fmt.Println("3 for in-order traversal")
+	fmt.Println("4 for level-order traversal")
+	var choice int
+	fmt.Scanf("%d", &choice)
+	switch choice {
+	case 1:
+		preorder(bt.root)
+		return
+	case 2:
+		postOrder(bt.root)
+		return
+	case 3:
+		inOrder(bt.root)
+		return
+	case 4:
+		bfs(bt.root)
+		return
+	default:
+		fmt.Println("Invalid choice please enter 1,2,3 or 4")
+	}
+
+}
+
+func preorder[T comparable](node *TreeNode[T]) {
+	if node == nil {
+		return
+	}
+	fmt.Println(node.data)
+	preorder(node.left)
+	preorder(node.right)
+}
+
+func postOrder[T comparable](node *TreeNode[T]) {
+	if node == nil {
+		return
+	}
+	preorder(node.left)
+	preorder(node.right)
+	fmt.Println(node.data)
+}
+
+func inOrder[T comparable](node *TreeNode[T]) {
+	if node == nil {
+		return
+	}
+	preorder(node.left)
+	fmt.Println(node.data)
+	preorder(node.right)
+}
+
+func bfs[T comparable](node *TreeNode[T]) {
+	if node == nil {
+		return
+	}
+	queue := make([]*TreeNode[T], 0)
+	queue = append(queue, node)
+
+	for len(queue) > 0 {
+		curr := queue[0]
+		queue = queue[1:]
+
+		fmt.Println(curr.data)
+
+		if curr.left != nil {
+			queue = append(queue, curr.left)
+		}
+
+		if curr.right != nil {
+			queue = append(queue, curr.right)
+		}
+	}
+}
+
+func (bt *BinaryTree[T]) GetLength() int {
+	if bt.root == nil {
+		return 0
+	}
+	return bt.root.GetLength()
+}
+
+func (node *TreeNode[T]) GetLength() int {
+	if node == nil {
+		return 0
+	}
+	leftH := 0
+	if node.left != nil {
+		leftH = node.left.GetLength()
+	}
+	rightH := 0
+	if node.right != nil {
+		rightH = node.right.GetLength()
+	}
+	return 1 + int(math.Max(float64(leftH), float64(rightH)))
+}
+
+func (bt *BinaryTree[T]) Delete(value T) {
+	bt.root = deleteNode(bt.root, value)
+}
+
+func deleteNode[T comparable](root *TreeNode[T], value T) *TreeNode[T] {
+	if root == nil {
+		return nil
+	}
+	if root.data == value {
+		if root.left == nil && root.right == nil {
+			return nil
+		}
+		if root.left == nil {
+			return root.right
+		}
+		if root.right == nil {
+			return root.left
+		}
+		return root.left
+	}
+	root.left = deleteNode(root.left, value)
+	root.right = deleteNode(root.right, value)
+	return root
+}
